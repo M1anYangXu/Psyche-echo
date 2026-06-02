6cca68457d58c30ee7d835df2e1b6a0a36521042
小绵羊 <1556855894@qq.com>
2026-06-02 16:08:17 +0800

    Initial commit

---
+++ src/test/java/com/miany/psycheecho/PsycheEchoPluginTest.java
@@ -0,0 +1,24 @@
+package com.miany.psycheecho;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import run.halo.app.plugin.PluginContext;
+
+@ExtendWith(MockitoExtension.class)
+class PsycheEchoPluginTest {
+
+    @Mock
+    PluginContext context;
+
+    @InjectMocks
+    PsycheEchoPlugin plugin;
+
+    @Test
+    void contextLoads() {
+        plugin.start();
+        plugin.stop();
+    }
+}
